5f1e241938b711b2bac8175187639601e35dc9eb
Kim-kym <msgim725@gmail.com>
2024-11-04 16:02:49 +0900

    for문

--- src/com/javaex/bagic/flow/LoopEx.java
+++ src/com/javaex/bagic/flow/LoopEx.java
@@ -6,7 +6,9 @@ public class LoopEx {
 
 	public static void main(String[] args) {
 //		whileEx();
-		whileGugu();
+//		whileGugu();
+//		doWhileEx();
+		forGugu();
 	}
 	
 //	private static void whileEx() {
@@ -36,6 +38,44 @@ public class LoopEx {
 				System.out.println( gugu +" * "+ i + " = " + i*gugu);
 				i += 1;
 			}
-			System.out.println("당신은 8단 마스터!");
+			System.out.println("당신은 구구단 마스터!");
+			scanner.close();
 	}
+		private static void doWhileEx( ) {
+			//	입력 받은 정수를 다 더한다 
+			//	입력 받은 정수가 0이면 반복문 종료
+			int total = 0;	//	합산 변수 
+			int value = 0; 	// 	더해주는 수, 반복제어변수 
+			
+			System.out.println("숫자를 입력하세여요. [0이면 종료]");
+			Scanner scanner = new Scanner(System.in);
+			
+			//	최소 1번 실행 
+			do {
+				value = scanner.nextInt();
+				total += value;
+				System.out.println("합계:" + total);
+			} while(value != 0);
+			
+			scanner.close();
+			}
+		private static void forGugu() {
+			//	단을 입력 받아서 해당 단의 구구표를 ㅜㄹ력
+			//	(for loop)
+			//	for (반복변수 초기화; 반복 조건 확인; 반복변수 증감)
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("단을 입력하세요.");
+		System.out.println("단:");
+		
+		int dan = scanner.nextInt();
+		
+		for (int i = 1; // 반복 변수 초기화
+			i <= 9; 	// 반복 조건 체크 
+			++i) {		// 반복 변수 증감 
+			System.out.println(dan + " * " + i + " = " + (dan * i));
+		}
+		
+		scanner.close();
+		}
+		
 }
