cd33c9c99ae37a9741dd5ae3573d96af22007f09
Kim-kym <msgim725@gmail.com>
2024-11-07 18:10:25 +0900

    song class ~ staic

--- src/com/javaex/oop/song/SongApp.java
+++ src/com/javaex/oop/song/SongApp.java
@@ -32,7 +32,7 @@ class Song {
 	public void setComposer(String composer) {
 		this.composer = composer; 
 	}
-	public int getYear () {
+	public int getYear() {
 		return year;
 	}
 	public void setYear (int year) {
@@ -44,11 +44,45 @@ class Song {
 	public void setTrack(int track) {
 		this.track = track;
 	}
+	public void showInfo() {
+		System.out.printf("%s, %s, ( %s, %d, %d번 track, %s 작곡 )%n",
+						artist, title, album, year, track, composer);
+		
+	}
 }
 
 public class SongApp {
 
 	public static void main(String[] args) {
+		Song singer1 = new Song();
+		singer1.setTitle("좋은날");
+		singer1.setArtist("아이유");
+		singer1.setAlbum("Real");
+		singer1.setComposer("이민수");
+		singer1.setYear(2010);
+		singer1.setTrack(3);
+		
+		singer1.showInfo();
+		
+		Song singer2 = new Song();
+		singer2.setTitle("거짓말");
+		singer2.setArtist("BIGBANG");
+		singer2.setAlbum("Always");
+		singer2.setComposer("G-DRAGON");
+		singer2.setYear(2007);
+		singer2.setTrack(2);
+		
+		singer2.showInfo();
+		
+		Song singer3 = new Song();
+		singer3.setTitle("벚꽃엔딩");
+		singer3.setArtist("버스커버스커");
+		singer3.setAlbum("버스커버스커1집");
+		singer3.setComposer("장범준");
+		singer3.setYear(2012);
+		singer3.setTrack(4);
+		
+		singer3.showInfo();
 
 	}
 
--- src/com/javaex/oop/staticmember/StaticExApp.java
+++ src/com/javaex/oop/staticmember/StaticExApp.java
@@ -3,7 +3,25 @@ package com.javaex.oop.staticmember;
 public class StaticExApp {
 
 	public static void main(String[] args) {
-
+		//	첫 번째 인스턴스가 생성되기 전에 클래스가 먼저 메모리에 로드 
+		// 	Static 블록에서 스태틱 영역 초기화 -> 생성자 호출
+		StaticEx s1 = new StaticEx();	//	인스턴스 생성 
+		System.out.println("refCount:" + StaticEx.refCount);	//	스태틱 영역에 있는 변수이기 때문에 new 선언하지 않아도 불러올 수 있다 
+		
+		StaticEx s2 = new StaticEx();	//	인스턴스 생성 
+		System.out.println("refCount:" + StaticEx.refCount);
+		
+		//	s1 참조 해제 
+		s1 = null;
+		
+		System.gc();	//	가비지 컬렉터 호출 -> 직접 호출은 사양하자 
+		try {
+			Thread.sleep(3000); //	3초간 대기
+			System.out.println("refCount:" + StaticEx.refCount);
+		} catch (Exception e) {
+			
+		}
+		
 	}
 
 }
