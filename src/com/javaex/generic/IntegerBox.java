271f7b77be76001bc6051fafd376fe0a23794dcc
Kim-kym <msgim725@gmail.com>
2024-11-12 13:59:02 +0900

    Generic

---
+++ src/com/javaex/generic/IntegerBox.java
@@ -0,0 +1,16 @@
+package com.javaex.generic;
+
+//Integer만 담을 수 있는 상자
+
+public class IntegerBox {
+		Integer content;
+
+		public Integer getContent() {
+			return content;
+		}
+
+		public void setContent(Integer content) {
+			this.content = content;
+		}
+
+}
